361957b27e9dfaa5da190c0101d1b951b8ab0f94
Denys Bushuliak <apollo128@gmail.com>
2021-02-19 18:20:30 +0300

    Исправил таблицу

--- kurmanci.online/components/TimeTable/TimeTable.tsx
+++ kurmanci.online/components/TimeTable/TimeTable.tsx
@@ -35,100 +35,95 @@ export function TimeTable(props: TimeTableProps) {
         {props.forms.future && (
           <>
             <tr className={"border"}>
-              <td rowSpan={6} className={"time-column border"}>
+              <td className={"time-column border"}>
                 <span>Буд.</span>
               </td>
-              <td rowSpan={6} className={"border"}>
+              <td className={"border"}>
                 Gelo
                 <br />
                 Ma
                 <br />
                 ... ?
               </td>
-              <td className={"border"}>ezê {props.forms.future["1_i"][0]}</td>
-              <td>ezê {props.forms.future["1_i"][1]}</td>
-            </tr>
-            <tr>
               <td className={"border"}>
+                ezê {props.forms.future["1_i"][0]}
+                <br />
                 tuyê {props.forms.future["2_you"][0]}
-              </td>
-              <td>tuyê {props.forms.future["2_you"][1]}</td>
-            </tr>
-            <tr>
-              <td className={"border"}>
+                <br />
                 ewê {props.forms.future["3_he_she_it"][0]}
-              </td>
-              <td>ewê {props.forms.future["3_he_she_it"][1]}</td>
-            </tr>
-            <tr>
-              <td className={"border"}>emê {props.forms.future["4_we"][0]}</td>
-              <td>emê {props.forms.future["4_we"][1]}</td>
-            </tr>
-            <tr>
-              <td className={"border"}>
+                <br />
+                emê {props.forms.future["4_we"][0]}
+                <br />
                 hûnê {props.forms.future["5_you_all"][0]}
-              </td>
-              <td>hûnê {props.forms.future["5_you_all"][1]}</td>
-            </tr>
-            <tr className={"border"}>
-              <td className={"border"}>
+                <br />
                 ewê {props.forms.future["6_they"][0]}
               </td>
-              <td>ewê {props.forms.future["6_they"][1]}</td>
+              <td>
+                ezê {props.forms.future["1_i"][1]}
+                <br />
+                tuyê {props.forms.future["2_you"][1]}
+                <br />
+                ewê {props.forms.future["3_he_she_it"][1]}
+                <br />
+                emê {props.forms.future["4_we"][1]}
+                <br />
+                hûnê {props.forms.future["5_you_all"][1]}
+                <br />
+                ewê {props.forms.future["6_they"][1]}
+                <br />
+              </td>
             </tr>
           </>
         )}
         {props.forms.present && (
           <>
-            <tr>
-              <td rowSpan={6} className={"time-column border"}>
+            <tr className={"border"}>
+              <td className={"time-column border"}>
                 <span>Наст.</span>
               </td>
-              <td rowSpan={6} className={"border"}>
+              <td className={"border"}>
                 Gelo
                 <br />
                 Ma
                 <br />
                 ... ?
               </td>
-              <td className={"border"}>ez {props.forms.present["1_i"][0]}</td>
-              <td>ez {props.forms.present["1_i"][1]}</td>
-            </tr>
-            <tr>
-              <td className={"border"}>tu {props.forms.present["2_you"][0]}</td>
-              <td>tu {props.forms.present["2_you"][1]}</td>
-            </tr>
-            <tr>
               <td className={"border"}>
+                ez {props.forms.present["1_i"][0]}
+                <br />
+                tu {props.forms.present["2_you"][0]}
+                <br />
                 ew {props.forms.present["3_he_she_it"][0]}
-              </td>
-              <td>ew {props.forms.present["3_he_she_it"][1]}</td>
-            </tr>
-            <tr>
-              <td className={"border"}>em {props.forms.present["4_we"][0]}</td>
-              <td>em {props.forms.present["4_we"][1]}</td>
-            </tr>
-            <tr>
-              <td className={"border"}>
+                <br />
+                em {props.forms.present["4_we"][0]}
+                <br />
                 hûn {props.forms.present["5_you_all"][0]}
-              </td>
-              <td>hûn {props.forms.present["5_you_all"][1]}</td>
-            </tr>
-            <tr className={"border"}>
-              <td className={"border"}>
+                <br />
                 ew {props.forms.present["6_they"][0]}
               </td>
-              <td>ew {props.forms.present["6_they"][1]}</td>
+              <td>
+                ez {props.forms.present["1_i"][1]}
+                <br />
+                tu {props.forms.present["2_you"][1]}
+                <br />
+                ew {props.forms.present["3_he_she_it"][1]}
+                <br />
+                em {props.forms.present["4_we"][1]}
+                <br />
+                hûn {props.forms.present["5_you_all"][1]}
+                <br />
+                ew {props.forms.present["6_they"][1]}
+              </td>
             </tr>
           </>
         )}
         {props.forms.past && (
           <>
             <tr>
-              <td rowSpan={6} className={"time-column border"}>
+              <td className={"time-column border"}>
                 <span>Прош.</span>
               </td>
-              <td rowSpan={6} className={"border"}>
+              <td className={"border"}>
                 Gelo
                 <br />
                 Ma
@@ -138,58 +133,38 @@ export function TimeTable(props: TimeTableProps) {
               <td className={"border"}>
                 {props.verbIs === "intransitive" ? "ez" : "min"}{" "}
                 {props.forms.past["1_i"][0]}
-              </td>
-              <td>
-                {props.verbIs === "intransitive" ? "ez" : "min"}{" "}
-                {props.forms.past["1_i"][1]}
-              </td>
-            </tr>
-            <tr>
-              <td className={"border"}>
+                <br />
                 {props.verbIs === "intransitive" ? "tu" : "te"}{" "}
                 {props.forms.past["2_you"][0]}
-              </td>
-              <td>
-                {props.verbIs === "intransitive" ? "tu" : "te"}{" "}
-                {props.forms.past["2_you"][1]}
-              </td>
-            </tr>
-            <tr>
-              <td className={"border"}>
+                <br />
                 {props.verbIs === "intransitive" ? "ew" : "wî/wê"}{" "}
                 {props.forms.past["3_he_she_it"][0]}
-              </td>
-              <td>
-                {props.verbIs === "intransitive" ? "ew" : "wî/wê"}{" "}
-                {props.forms.past["3_he_she_it"][1]}
-              </td>
-            </tr>
-            <tr>
-              <td className={"border"}>
+                <br />
                 {props.verbIs === "intransitive" ? "em" : "me"}{" "}
                 {props.forms.past["4_we"][0]}
-              </td>
-              <td>
-                {props.verbIs === "intransitive" ? "em" : "me"}{" "}
-                {props.forms.past["4_we"][1]}
-              </td>
-            </tr>
-            <tr>
-              <td className={"border"}>
+                <br />
                 {props.verbIs === "intransitive" ? "hûn" : "we"}{" "}
                 {props.forms.past["5_you_all"][0]}
-              </td>
-              <td>
-                {props.verbIs === "intransitive" ? "hûn" : "we"}{" "}
-                {props.forms.past["5_you_all"][1]}
-              </td>
-            </tr>
-            <tr>
-              <td className={"border"}>
+                <br />
                 {props.verbIs === "intransitive" ? "ew" : "wan"}{" "}
                 {props.forms.past["6_they"][0]}
               </td>
               <td>
+                {props.verbIs === "intransitive" ? "ez" : "min"}{" "}
+                {props.forms.past["1_i"][1]}
+                <br />
+                {props.verbIs === "intransitive" ? "tu" : "te"}{" "}
+                {props.forms.past["2_you"][1]}
+                <br />
+                {props.verbIs === "intransitive" ? "ew" : "wî/wê"}{" "}
+                {props.forms.past["3_he_she_it"][1]}
+                <br />
+                {props.verbIs === "intransitive" ? "em" : "me"}{" "}
+                {props.forms.past["4_we"][1]}
+                <br />
+                {props.verbIs === "intransitive" ? "hûn" : "we"}{" "}
+                {props.forms.past["5_you_all"][1]}
+                <br />
                 {props.verbIs === "intransitive" ? "ew" : "wan"}{" "}
                 {props.forms.past["6_they"][1]}
               </td>
@@ -199,10 +174,10 @@ export function TimeTable(props: TimeTableProps) {
         {props.forms.custom && (
           <>
             <tr>
-              <td rowSpan={6} className={"time-column border"}>
+              <td className={"time-column border"}>
                 <span>{props.customRowGroupName}</span>
               </td>
-              <td rowSpan={6} className={"border"}>
+              <td className={"border"}>
                 Gelo
                 <br />
                 Ma
@@ -212,58 +187,41 @@ export function TimeTable(props: TimeTableProps) {
               <td className={"border"}>
                 {props.verbIs === "intransitive" ? "ez" : "min"}{" "}
                 {props.forms.custom["1_i"][0]}
+                <br />
+                {props.verbIs === "intransitive" ? "tu" : "te"}{" "}
+                {props.forms.custom["2_you"][0]}
+                <br />
+                {props.verbIs === "intransitive" ? "ew" : "wî/wê"}{" "}
+                {props.forms.custom["3_he_she_it"][0]}
+                <br />
+                {props.verbIs === "intransitive" ? "em" : "me"}{" "}
+                {props.forms.custom["4_we"][0]}
+                <br />
+                {props.verbIs === "intransitive" ? "hûn" : "we"}{" "}
+                {props.forms.custom["5_you_all"][0]}
+                <br />
+                {props.verbIs === "intransitive" ? "ew" : "wan"}{" "}
+                {props.forms.custom["6_they"][0]}
               </td>
               <td>
                 {props.verbIs === "intransitive" ? "ez" : "min"}{" "}
                 {props.forms.custom["1_i"][1]}
-              </td>
-            </tr>
-            <tr>
-              <td className={"border"}>
-                {props.verbIs === "intransitive" ? "tu" : "te"}{" "}
-                {props.forms.custom["2_you"][0]}
-              </td>
-              <td>
+                <br />
                 {props.verbIs === "intransitive" ? "tu" : "te"}{" "}
                 {props.forms.custom["2_you"][1]}
-              </td>
-            </tr>
-            <tr>
-              <td className={"border"}>
-                {props.verbIs === "intransitive" ? "ew" : "wî/wê"}{" "}
-                {props.forms.custom["3_he_she_it"][0]}
-              </td>
-              <td>
+                <br />
                 {props.verbIs === "intransitive" ? "ew" : "wî/wê"}{" "}
                 {props.forms.custom["3_he_she_it"][1]}
-              </td>
-            </tr>
-            <tr>
-              <td className={"border"}>
-                {props.verbIs === "intransitive" ? "em" : "me"}{" "}
-                {props.forms.custom["4_we"][0]}
-              </td>
-              <td>
+                <br />
                 {props.verbIs === "intransitive" ? "em" : "me"}{" "}
                 {props.forms.custom["4_we"][1]}
-              </td>
-            </tr>
-            <tr>
-              <td className={"border"}>
-                {props.verbIs === "intransitive" ? "hûn" : "we"}{" "}
-                {props.forms.custom["5_you_all"][0]}
-              </td>
-              <td>
+                <br />
                 {props.verbIs === "intransitive" ? "ew" : "wan"}{" "}
                 {props.forms.custom["5_you_all"][1]}
-              </td>
-            </tr>
-            <tr>
-              <td className={"border"}>
+                <br />
                 {props.verbIs === "intransitive" ? "ew" : "wan"}{" "}
-                {props.forms.custom["6_they"][0]}
+                {props.forms.custom["6_they"][1]}
               </td>
-              <td> {props.forms.custom["6_they"][1]}</td>
             </tr>
           </>
         )}
