c72fee91c3b10ee6d473ef0209cbd3b18e816942
Denys Bushuliak <apollo128@gmail.com>
2021-02-19 14:22:40 +0300

    Исправил ошибку в index.js

--- kurmanci.online/src/pages/index.js
+++ kurmanci.online/src/pages/index.js
@@ -1,45 +1,38 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
 const features = [
   {
-    title: 'Граматика',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    title: "Граматика",
+    imageUrl: "img/undraw_docusaurus_mountain.svg",
     description: (
       <>
-        На этом ресурсе вы найдете правила граматики курдского языка изложенный в порядке лучшего его усвоения.
+        На этом ресурсе вы найдете правила граматики курдского языка изложенный
+        в порядке лучшего его усвоения.
       </>
     ),
   },
   {
-    title: 'С примерами',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
-    description: (
-      <>
-        Примеры взяты с реальных публикаций.
-      </>
-    ),
+    title: "С примерами",
+    imageUrl: "img/undraw_docusaurus_tree.svg",
+    description: <>Примеры взяты с реальных публикаций.</>,
   },
   {
-    title: 'Стандартный диалект',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Более-менее стандартный и понятный диалект этого языка.
-      </>
-    ),
+    title: "Стандартный диалект",
+    imageUrl: "img/undraw_docusaurus_react.svg",
+    description: <>Более-менее стандартный и понятный диалект этого языка.</>,
   },
 ];
 
-function Feature({imageUrl, title, description}) {
+function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={clsx('col col--4', styles.feature)}>
+    <div className={clsx("col col--4", styles.feature)}>
       {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
@@ -53,21 +46,21 @@ function Feature({imageUrl, title, description}) {
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <Layout title={`Hello from ${siteConfig.title}`}>
+      <header className={clsx("hero hero--primary", styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
             <Link
               className={clsx(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
+                "button button--outline button--secondary button--lg",
+                styles.getStarted
               )}
-              to={useBaseUrl('docs/chapter-1/pronouns')}>
+              to={useBaseUrl("docs/chapter-1/pronouns")}
+            >
               Начинаем
             </Link>
           </div>
