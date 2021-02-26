af5556ba29364469a09d5ec17cbfd46f030fd0bd
Denys Bushuliak <apollo128@gmail.com>
2021-02-26 18:34:33 +0300

    Добавили xwendin

--- kurmanci.online/sidebars.js
+++ kurmanci.online/sidebars.js
@@ -43,6 +43,7 @@ module.exports = {
           "chapter-2/examples/rakirin",
           "chapter-2/examples/rabun",
           "chapter-2/examples/nivisin",
+          "chapter-2/examples/xwendin",
         ],
       },
     ],
