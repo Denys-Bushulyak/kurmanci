6e6ae070e32212294ab89a7f2abd66234138022f
Denys Bushuliak <apollo128@gmail.com>
2021-03-28 21:00:37 +0300

    объеденил словообразование

--- kurmanci.online/sidebars.js
+++ kurmanci.online/sidebars.js
@@ -78,10 +78,6 @@ module.exports = {
         Примеры: ["chapter-4/examples/mezin-buyin"],
       },
     ],
-    "Глава 5": [
-      "chapter-5/numerics",
-      "chapter-5/word-creation",
-      "chapter-5/word-creation-2",
-    ],
+    "Глава 5": ["chapter-5/numerics", "chapter-5/word-creation"],
   },
 };
