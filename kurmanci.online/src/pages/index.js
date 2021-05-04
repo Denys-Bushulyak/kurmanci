1f9b78bce691efa69bbb87440581fb852cf2641f
Denys Bushuliak <apollo128@gmail.com>
2021-05-04 21:09:03 +0300

    Новая глава

--- kurmanci.online/src/pages/index.js
+++ kurmanci.online/src/pages/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import clsx from "clsx";
-import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import useBaseUrl from "@docusaurus/useBaseUrl";
-import styles from "./styles.module.css";
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
 
 const features = [
   {
-    title: "Граматика",
-    imageUrl: "img/undraw_docusaurus_mountain.svg",
+    title: 'Граматика',
+    imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
         На этом ресурсе вы найдете правила граматики курдского языка изложенный
@@ -18,13 +18,13 @@ const features = [
     ),
   },
   {
-    title: "С примерами",
-    imageUrl: "img/undraw_docusaurus_tree.svg",
+    title: 'С примерами',
+    imageUrl: 'img/undraw_docusaurus_tree.svg',
     description: <>Примеры взяты с реальных публикаций.</>,
   },
   {
-    title: "Стандартный диалект",
-    imageUrl: "img/undraw_docusaurus_react.svg",
+    title: 'Стандартный диалект',
+    imageUrl: 'img/undraw_docusaurus_react.svg',
     description: <>Более-менее стандартный и понятный диалект этого языка.</>,
   },
 ];
@@ -32,7 +32,7 @@ const features = [
 function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={clsx("col col--4", styles.feature)}>
+    <div className={clsx('col col--4', styles.feature)}>
       {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
@@ -49,7 +49,7 @@ function Home() {
   const { siteConfig = {} } = context;
   return (
     <Layout title={`Граматика языка ${siteConfig.title}`}>
-      <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
@@ -57,16 +57,16 @@ function Home() {
           <div className={styles.buttons}>
             <Link
               className={clsx(
-                "button button--outline button--secondary button--lg",
+                'button button--outline button--secondary button--lg',
                 styles.getStarted
               )}
-              to={useBaseUrl("docs/chapter-1/pronouns")}
+              to={useBaseUrl('docs/chapter-1/pronouns')}
             >
               Начинаем
             </Link>
           </div>
-          <p style={{ textAlign: "center", marginTop: "1em" }}>
-            Последнее обновление было 03.05.2021
+          <p style={{ textAlign: 'center', marginTop: '1em' }}>
+            Последнее обновление было 04.05.2021
           </p>
         </div>
       </header>
